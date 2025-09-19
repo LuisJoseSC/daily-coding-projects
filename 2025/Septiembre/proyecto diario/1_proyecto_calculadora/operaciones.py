8e4c9ed3e1ee20b0b99b5b293f742dbc197683ea
LuisJoseSC <lj.sanchezc1@uniandes.edu.co>
2025-09-19 14:28:22 -0500

    archivos y esqueleto

---
+++ 2025/Septiembre/proyecto diario/1_proyecto_calculadora/operaciones.py
@@ -0,0 +1,8 @@
+def suma (numero_1: int, numero_2: int)-> int:
+    pass
+def resta (numero_1: int, numero_2: int) -> int:
+    pass
+def multiplicacion (numero_1: int, numero_2: int) -> int:
+    pass
+def division (numero_1: int, numero_2: int) -> int:
+    pass
